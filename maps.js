7867f01c8e4370bc006f959d4107c0238a0638de
Adam Underhill <adameunderhill@gmail.com>
2021-05-27 17:08:05 -0700

    creating new array based on function of existing array

---
+++ maps.js
@@ -0,0 +1,38 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+    const results = [];
+    for (let item of array) {
+      results.push(callback(item));
+
+
+    }
+  return results
+}
+
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+  for (let i = arr1.length; i--;) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`🟢🟢🟢 The Arrays " ${arr1}, " and " ${arr2} " are the same`);
+  } else {
+    console.log(`🔴🔴🔴 The Arrays " ${arr1}, " and " ${arr2} " are the same`)
+  }
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ])
