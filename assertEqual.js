75ea3d877d686f59ffcccc2a31c604475cbd4072
Adam Underhill <adameunderhill@gmail.com>
2021-05-31 11:40:30 -0700

    console message if arrays are equal

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: [${actual}] === [${expected}]`)
+  } else if (actual !== expected) {
+    console.log(`🔴🔴🔴Assertion Failed: [${actual}] !== [${expected}]`)
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"))
