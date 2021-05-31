e778fa23f46d26d9bba204c4ac24b5812e325aad
Adam Underhill <adameunderhill@gmail.com>
2021-05-31 16:48:11 -0700

    refactored function for end of array

--- tail.js
+++ tail.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `🟢🟢🟢Assertion Passed: [${actual}] === [${expected}]`
-  } else if (actual !== expected) {
-    return `🔴🔴🔴Assertion Failed: [${actual}] !== [${expected}]`
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const tail = function(arr) {
   return arr.slice(1)
 }
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
+module.export = tail
