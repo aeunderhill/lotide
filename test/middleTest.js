0115e2eae53ebd357aded1863e6836122de49a07
Adam Underhill <adameunderhill@gmail.com>
2021-05-31 18:24:52 -0700

    chai added to tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,1 +1,18 @@
-const middle   = require('../middle');
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+
+describe("#middle", () => {
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it("returns [4] for [1,2,3,4,5,7,8]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,7,8]), [4]);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,19 @@
+const assert = require('chai').assert
 const tail = require('../tail')
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
+
+
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
+
+});
+
+
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(result[0], 'Lighthouse');
+
+  });
