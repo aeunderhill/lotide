5520bccea241907772c83f7deef396615465413d
Adam Underhill <adameunderhill@gmail.com>
2021-05-31 17:47:36 -0700

    refactored for middle of array

--- middle.js
+++ middle.js
@@ -1,24 +1,7 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length)
-    return false;
-  for (let i = arr1.length; i--;) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
+const eqArrays = require('./eqArrays');
 
-
-const assertArraysEqual = function(arr1, arr2) {
-  let result = eqArrays(arr1, arr2);
-  if (result) {
-    console.log("🟢🟢🟢 The Arrays ", arr1, " and ", arr2, " are the same");
-  } else {
-    console.log("🔴🔴🔴 The Arrays ", arr1, " and ", arr2, " are the same")
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const middle = function(arr) {
@@ -34,4 +17,4 @@ const middle = function(arr) {
 }
 }
 
-
+module.export = middle
